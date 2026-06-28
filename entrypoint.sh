72dac2eb5d3650cddd6889999d6b9884429b876a
sentryrec <sentryrec@gmail.com>
2026-06-28 20:30:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-mkdir -p /var/www/html
+MYBB_VERSION=${MYBB_VERSION:-1840}
 
-chown -R www-data:www-data /var/www/html
+if [ ! -f /var/www/html/index.php ]; then
+    echo "Installing MyBB ${MYBB_VERSION}..."
+
+    wget -q https://resources.mybb.com/downloads/mybb_${MYBB_VERSION}.zip -O /tmp/mybb.zip
+
+    unzip -q /tmp/mybb.zip -d /tmp
+
+    cp -R /tmp/Upload/* /var/www/html/
+
+    chown -R www-data:www-data /var/www/html
+
+    rm -rf /tmp/Upload /tmp/mybb.zip
+fi
 
 exec "$@"
